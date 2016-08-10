75eba4fa8d1206b29309f2c1b0f0ce6afc8a0c16
Carl Brando <Carl Brando>
2016-08-09 20:10:08 -0400

    modified:   Jewel/bin/tbc/server/ClientInstance.class 	modified:   Jewel/bin/tbc/server/JewelServer.class 	modified:   Jewel/src/tbc/server/ClientInstance.java 	modified:   Jewel/src/tbc/server/JewelServer.java

--- Jewel/src/tbc/server/ClientInstance.java
+++ Jewel/src/tbc/server/ClientInstance.java
@@ -1,29 +1,48 @@
 package tbc.server;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ClientInstance implements Runnable{
 
 	
 	JewelServer server;
+	Socket soc;
 	ObjectOutputStream oos;
 	ObjectInputStream ois;
 	int id;
+	boolean connected = true;
 	
-	public ClientInstance(JewelServer server) {
+	public ClientInstance(JewelServer server, Socket cliSoc) {
 		this.server = server;
+		soc = cliSoc;
 	}
 	
 	@Override
 	public void run() {
-		init();
-		
+		try{
+			init();
+			while(connected && server.running){
+				
+			}
+			
+			
+		}catch(IOException e){
+			e.printStackTrace();
+			System.err.println("Client failed! ");
+		}
 	}
 	
-	private void init(){
+	private void init() throws IOException{ 
 		
+		oos = new ObjectOutputStream(soc.getOutputStream());
+		ois = new ObjectInputStream(soc.getInputStream());
+		
+		server.addClient(this);
 	}
 	
 	void setID(int id){
--- Jewel/src/tbc/server/JewelServer.java
+++ Jewel/src/tbc/server/JewelServer.java
@@ -1,32 +1,47 @@
 package tbc.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 
 public class JewelServer {
 
 	HashMap<Integer, ClientInstance> clients = new HashMap<Integer, ClientInstance>();
-	Socket server;
+	ServerSocket server;
+	boolean running = true;
 	
 	public static void main(String[] args) {
 		new JewelServer();
 	}
 
 	public JewelServer() {
-
-	}
-	
-	synchronized void addClient(int id,ClientInstance ci){
-		clients.put(id, ci);
+		try {
+			server = new ServerSocket(9999);
+			while(running){
+				Socket cliSoc = server.accept();
+				ClientInstance ci = new ClientInstance(this, cliSoc);
+				(new Thread(ci)).start();
+			}
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
 		
 	}
+	synchronized void addClient(ClientInstance ci){
+		int id = genID();
+		clients.put(id, ci);
+		ci.setID(id);
+		//Add to game world
+		//Send packets to everyone about the new client
+	}
 	
-	private void genID(){
+	private int genID(){
 		int id = 0;
 		while(clients.containsKey(id)){
 			id++;
 		}
-		
+		return id;
 	}
 
 
